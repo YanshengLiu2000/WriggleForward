0442a03c2a69979415c901a41c8dd715cb1bbbf7
Yansheng Liu <yansheng.liu2000@gmail.com>
2018-07-23 16:46:46 +1000

    check Bilibili and fail, again

--- WebSpider/download.py
+++ WebSpider/download.py
@@ -1,3 +1,6 @@
+import asyncio
+import threading
+
 import requests
 
 # IMAGE_URL='https://morvanzhou.github.io/static/img/description/learning_step_flowchart.png'
@@ -12,4 +15,31 @@ import requests
 #         f.write(chunk)
 
 
+#yield again
+# import time
+#
+# def yield_test(n):
+#     print('This is yield_test:')
+#     for i in range(n):
+#         print('i=',i)
+#         yield call(i)
+#         time.sleep(2)
+#         print('====================')
+#     print('Do something!')
+#     print('end!')
+# def call(i):
+#     return 'This is CALL,i =='+str(i+1)
+#
+# for i in yield_test(5):
+#     print(i,',')
 
+# 异步IO例子：适配Python3.5，使用async和await关键字
+async def hello(index):       # 通过关键字async定义协程
+    print('Hello world! index=%s, thread=%s' % (index, threading.currentThread()))
+    await asyncio.sleep(1)     # 模拟IO任务
+    print('Hello again! index=%s, thread=%s' % (index, threading.currentThread()))
+
+loop = asyncio.get_event_loop()     # 得到一个事件循环模型
+tasks = [hello(1)]        # 初始化任务列表
+loop.run_until_complete(asyncio.wait(tasks))    # 执行任务
+loop.close()
